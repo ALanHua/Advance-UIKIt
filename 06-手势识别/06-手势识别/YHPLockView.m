e97275f0e0b96197d1b6b660bdaf0e1925891a51
Andy <smrtwater@126.com>
2016-05-08 23:15:05 +0800

    手势识别

--- 06-手势识别/06-手势识别/YHPLockView.m
+++ 06-手势识别/06-手势识别/YHPLockView.m
@@ -8,7 +8,63 @@
 
 #import "YHPLockView.h"
 
+@interface YHPLockView ()
+/**  */
+@property(nonatomic,strong)NSMutableArray* selectedBtn;
+/**  */
+@property(nonatomic,assign)CGPoint curP;
+@end
+
 @implementation YHPLockView
+
+-(NSMutableArray*)selectedBtn
+{
+    if (_selectedBtn == nil) {
+        _selectedBtn = [NSMutableArray array];
+    }
+    
+    return _selectedBtn;
+}
+
+
+
+-(IBAction)pan:(UIPanGestureRecognizer*)pan
+{
+//  获取触摸点
+     _curP = [pan locationInView:self];
+//  判断点是否在按钮上
+    for (UIButton* btn in self.subviews) {
+        // 判断点在不在一个矩形范围类
+        if( CGRectContainsPoint(btn.frame, _curP)
+        && btn.selected == NO){
+            //  点在按钮上
+            btn.selected = YES;
+            //  保存数组中
+            [self.selectedBtn addObject:btn];
+        }
+    }
+    //  重绘
+    [self setNeedsDisplay];
+    
+    if (pan.state == UIGestureRecognizerStateEnded) {
+//        创建可变字符串
+        NSMutableString* strM = [NSMutableString string];
+//       保存输入密码
+        for (UIButton* btn in self.selectedBtn) {
+            [strM appendFormat:@"%ld",btn.tag];
+        }
+        NSLog(@"%@",strM);
+        
+        // 还原界面
+//       [self.selectedBtn makeObjectsPerformSelector:@selector(setSelected:) withObject:@(NO)];// 该方法好像不起作用
+        for (UIButton* btn in self.selectedBtn) {
+            btn.selected = NO;
+        }
+        [self.selectedBtn removeAllObjects];
+        //  重绘
+//        [self setNeedsDisplay];
+    }
+}
 
 // 加载完xib的时候调用
 - (void)awakeFromNib
@@ -16,10 +72,13 @@
     // 创建9个按钮
     for ( int i = 0; i < 9; i++) {
         UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+        // 不允许用户交互，按钮不能点击，也就是不能高亮
+        btn.userInteractionEnabled = NO;
         
         [btn setImage:[UIImage imageNamed:@"gesture_node_normal"] forState:UIControlStateNormal];
         
         [btn setImage:[UIImage imageNamed:@"gesture_node_highlighted"] forState:UIControlStateSelected];
+        btn.tag = i;
         
         [self addSubview:btn];
     }
@@ -52,6 +111,34 @@
         btn.frame = CGRectMake(x, y, w, h);
         
     }
+    
+}
+#pragma mark - 绘图连线
+- (void)drawRect:(CGRect)rect
+{
+    
+    if (self.selectedBtn.count == 0) {
+        return;
+    }
+//    所有按钮中心点连线
+    UIBezierPath* path = [UIBezierPath bezierPath];
+    NSInteger count = self.selectedBtn.count;
+    for (int i = 0; i < count; i++) {
+        UIButton* btn = self.selectedBtn[i];
+        if (i == 0) {
+           //    设置起点
+            [path moveToPoint:btn.center];
+        }else{
+            [path addLineToPoint:btn.center];
+        }
+    }
+//   连线到手指的触摸点
+    [path addLineToPoint:_curP];
+    path.lineJoinStyle = kCGLineJoinRound;
+//    渲染
+    [[UIColor greenColor]set];
+    path.lineWidth = 5;
+    [path stroke];
     
 }
 

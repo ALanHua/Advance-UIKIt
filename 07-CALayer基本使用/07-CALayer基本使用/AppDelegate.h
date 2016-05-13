3961c619085cd56985ef10be936ee535e59e610c
Andy <smrtwater@126.com>
2016-05-14 00:48:51 +0800

    添加CALayer工程

---
+++ 07-CALayer基本使用/07-CALayer基本使用/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  07-CALayer基本使用
+//
+//  Created by yhp on 16/5/14.
+//  Copyright © 2016年 YouHuaPei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

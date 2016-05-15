9768073a634ab0c5390788834df7e10c785734aa
Andy <smrtwater@126.com>
2016-05-15 18:47:32 +0800

    CALayer基本使用

--- 07-CALayer基本使用/07-CALayer基本使用/ViewController.m
+++ 07-CALayer基本使用/07-CALayer基本使用/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *redView;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -16,8 +18,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+   
 }
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    //    图层形变
+    //    缩放
+    
+    [UIView animateWithDuration:1 animations:^{
+//      _redView.layer.transform = CATransform3DMakeRotation(M_PI, 0, 1, 0);
+//        _redView.layer.transform = CATransform3DMakeScale(0.5, 0.5, 1);
+    //    快速进行图层缩放，KVC
+//        [_redView.layer setValue:@0.5 forKeyPath:@"transform.scale"];
+    
+    }];
+    
+    
+}
+
+-(void)imageViewLayer
+{
+    //   cornerRadius设置控件的主层边框
+    _imageView.layer.cornerRadius = 50;
+    NSLog(@"%@",_imageView.layer.contents);
+    //   超出主层边框的内容全部裁剪掉
+    _imageView.layer.masksToBounds = YES;
+    //    设置边宽
+    _imageView.layer.borderWidth = 5;
+    _imageView.layer.borderColor = [[UIColor yellowColor]CGColor];
+    //   如何判断以后是否需要裁剪图片，就判断下需要显示图片控件是否是正方形
+}
+
+-(void)viewLayer
+{
+    // Opacity 不透明
+    _redView.layer.shadowOpacity = 1;
+    //    _redView.layer.shadowOffset   = CGSizeMake(10, 10);
+    //  涂成的颜色都是核心绘图框架的，通常CGColor
+    _redView.layer.shadowColor = [UIColor yellowColor].CGColor;
+    _redView.layer.shadowRadius = 10;
+    //  圆角半径
+    _redView.layer.cornerRadius = 50;
+    // 边宽
+    _redView.layer.borderWidth = 1;
+    _redView.layer.borderColor = [[UIColor whiteColor] CGColor];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];

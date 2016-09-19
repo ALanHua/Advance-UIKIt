2bad15939a7bb338e21b537a237da36c12c1f589
Andy <smrtwater@126.com>
2016-09-19 22:45:44 +0800

    键盘处理属性

--- 10-键盘处理/10-键盘处理/ViewController.m
+++ 10-键盘处理/10-键盘处理/ViewController.m
@@ -17,9 +17,43 @@
 
 @implementation ViewController
 
+/**
+ *  监听toobar上的按钮点击
+ */
+- (IBAction)previous:(UIBarButtonItem *)sender {
+    
+}
+- (IBAction)next:(UIBarButtonItem *)sender {
+    
+}
+- (IBAction)done:(UIBarButtonItem *)sender {
+    
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+   
+    // 加载工具条控件
+   UIToolbar* toolbar =  [[[NSBundle mainBundle] loadNibNamed:@"YHPkeyboard" owner:self options:nil]firstObject];
+    // 设置工具条
+    self.nameFeild.inputAccessoryView = toolbar;
+    self.emailFeild.inputAccessoryView = toolbar;
+    self.addressFeild.inputAccessoryView = toolbar;
+
+}
+-(void)testInputViewAndInputAccessoryView
+{
+    // 更换键盘
+    UIView* keyboard = [[UIView alloc]init];
+    keyboard.frame = CGRectMake(0, 0, 0, 100);
+    keyboard.backgroundColor = [UIColor redColor];
+    self.emailFeild.inputView = keyboard;
+    
+    //  设置键盘顶部工具条
+    UIView* toolbar = [[UIView alloc]init];
+    toolbar.frame = CGRectMake(0, 0, 0, 44);
+    toolbar.backgroundColor = [UIColor greenColor];
+    self.nameFeild.inputAccessoryView = toolbar;
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
@@ -34,15 +68,14 @@
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
     if (textField == self.nameFeild) {
-        
+        // 让emailFeild成为第一响应者
+        [self.emailFeild becomeFirstResponder];
     }else if ( textField == self.emailFeild){
-        
+        // 让addressFeild成为第一响应者
+        [self.addressFeild becomeFirstResponder];
     }else if (textField == self.addressFeild){
-//        [self.view endEditing:YES];
-        /**
-         *  第二种方法
-         */
-        [textField resignFirstResponder];
+        [self.view endEditing:YES];
+//        [textField resignFirstResponder];
     }
     
     return YES;
